232200e8a6c7f5e77d1551dbe81842da8c3bb314
Latricia <tricianickelberry@gmail.com>
2020-10-06 09:28:27 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+// const cards = (["Jason", "liz", "Terry"]);
+
+function writeCards(cards) {
+    for(let i = 0; i < cards.length; i++) {
+        console.log(`Thank you ${cards[i]} for the wonderful birthday cards`);
+    }
+    return cards;
+}
+
+    // writeCards(cards);
